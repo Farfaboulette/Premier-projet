8ed4d2f5cd8e4cc0ad09c866ff4c7bc433b81bb3
Princep David (HES) <david.princep@hes.adhes.hesge.ch>
2019-09-18 17:50:13 +0200

    ma première sauvegarde

---
+++ Bonjour.py
@@ -0,0 +1,14 @@
+print("bonjour !")
+
+audrey = 17
+marie = (audrey * 2) + 6.75
+max = marie - 9.27
+laura = max - 7.29
+jean = audrey + 8
+
+print("distance parcouru par audrey :" ,audrey, "Km")
+print("distance parcouru par marie :" ,marie, "Km")
+print("distance parcouru par max :" ,max, "Km")
+print("distance parcouru par laura :" ,laura, "Km")
+print("distance parcouru par jean :" ,jean, "Km")
+print("audrey pue la mort")
